5bd8adc53f44f0a56fc31f929fc3c42b3a71589a
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-04-02 11:30:53 -0600

    added js created list

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,32 @@
-var searchBox = document.getElementById("form1").value;
+var searchBox = document.getElementById("form1");
 var searchButton = document.getElementById("search-btn");
 var randButton = document.getElementById("random-btn");
+var listArray = document.getElementById("instructions");
 
 
 function searchByName(){
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+searchBox)
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+searchBox.value)
+                 
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data){
+       
+        for (var i = 0; i < data.drinks.length; i++) {
+            var instructionItem = document.createElement("li");
+            instructionItem.textContent = data.drinks[i].strDrink;
+            listArray.appendChild(instructionItem);
+            console.log(data.drinks[i].strDrink);
+        }
+        console.log(data);
+    })
+    
+    
+
+}
+
+function searchRandom(){
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
                  
     .then(function(response) {
         return response.json();
@@ -13,8 +35,11 @@ function searchByName(){
         console.log(data);
     })
     
+    
 
 }
 
+
+randButton.addEventListener("click", searchRandom);
 searchButton.addEventListener("click", searchByName);
 
