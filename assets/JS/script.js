2b5b6600f7d80399ad3207ca3c4d452976f383b1
Micah Diaz <micah.diaz@du.edu>
2022-04-02 14:39:32 -0600

    added html and js for result

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,8 @@
 var searchBox = document.getElementById("form1");
 var searchButton = document.getElementById("search-btn");
 var randButton = document.getElementById("random-btn");
-var listArray = document.getElementById("instructions");
-var details = document.getElementById("drink-details");
-var testButton = document.getElementById("test");
-var detailName = document.getElementById("drink-name");
+var resultMain = document.getElementById("result-main");
+
 
 function searchByName(){
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s="+searchBox.value)
@@ -13,14 +11,27 @@ function searchByName(){
         return response.json();
     })
     .then(function(data){
-       
-        for (var i = 0; i < data.drinks.length; i++) {
-            var instructionItem = document.createElement("li");
-            instructionItem.textContent = data.drinks[i].strDrink;
-            detailName.textContent=data.drinks[i].strDrink;
-            listArray.appendChild(instructionItem);
-            console.log(data.drinks[i].strDrink);
-        }
+        
+        var thumbnailMain = document.getElementById("thumbnail1");
+        var drinknameMain = document.getElementById("drink-name");
+        var instructionMain = document.getElementById("drink-instruction");
+        var drinkName = data.drinks[0].strDrink;
+        var drinkInstruction = data.drinks[0].strInstructions;
+        var drinkThumbnail = data.drinks[0].strDrinkThumb;
+        
+        thumbnailMain.src = drinkThumbnail;
+        drinknameMain.textContent = drinkName;
+        instructionMain.textContent = drinkInstruction;
+  
+
+
+        // for (var i = 0; i < data.drinks.length; i++) {
+    
+        //     instructionItem.textContent = data.drinks[i].strDrink;
+        //     // detailName.textContent=data.drinks[i].strDrink;
+        //     listArray.appendChild(instructionItem);
+        //     console.log(data.drinks[i].strDrink);
+        // }
         console.log(data);
     })
     
@@ -42,13 +53,13 @@ function searchRandom(){
 
 }
 
-function showHide(){
-    listArray.classList.add("hide");
-    details.classList.remove("hide");
-}
+// function showHide(){
+//     listArray.classList.add("hide");
+//     details.classList.remove("hide");
+// }
 
 
 randButton.addEventListener("click", searchRandom);
 searchButton.addEventListener("click", searchByName);
-testButton.addEventListener("click", showHide);
+// testButton.addEventListener("click", showHide);
 
